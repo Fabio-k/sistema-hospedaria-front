7490067d51d71f774625777a70bd479dbfba002c
fabio-k <fabiokmain@gmail.com>
2025-12-08 22:49:39 -0300

    feat: adiciona refresh automático do token

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,26 @@
 import NextAuth from "next-auth"
 import KeycloakProvider from "next-auth/providers/keycloak";
 import { decodeJwt } from "jose";
+import axios from "axios";
+
+const handleRefreshToken = async (token: any) => {
+  try {
+    const params = new URLSearchParams();
+    params.append("grant_type", "refresh_token");
+    params.append("client_id", process.env.KEYCLOAK_ID!);
+    params.append("client_secret", process.env.KEYCLOAK_SECRET!);
+    params.append("refresh_token", token.refreshToken);
+    const res = await axios.post(`${process.env.KEYCLOAK_ISSUER}/protocol/openid-connect/token`, params, {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    });
+    return res.data;
+  } catch (err) {
+    console.error("Failed to refresh token", err);
+    return null;
+  }
+}
 
 const handler = NextAuth({
   providers: [
@@ -14,7 +34,7 @@ const handler = NextAuth({
   async jwt({ token, account }) {
     if (account) {
       token.accessToken = account.access_token
-      token.accessTokenExpires = account.expires_at! * 1000
+      token.accessTokenExpires = account.expires_at! * 1000 - 60 * 1000;
       token.refreshToken = account.refresh_token!
 
       const decoded: any = decodeJwt(token.accessToken!)
@@ -26,6 +46,14 @@ const handler = NextAuth({
 
     if((token.accessTokenExpires as number) > Date.now()) {
       return token;
+    }
+
+    const refreshedToken = await handleRefreshToken(token);
+    if(refreshedToken) {
+      console.log("Token refreshed", refreshedToken);
+      token.accessToken = refreshedToken.access_token;
+      token.accessTokenExpires = Date.now() + refreshedToken.expires_in! * 1000 - 60 * 1000;
+      token.refreshToken = refreshedToken.refresh_token!;
     }
 
     return token;
